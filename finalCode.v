671450d864ee03792a3efa9d8ec363a1c52edd9b
Oscar Guzmán <60590466+odguzmanv@users.noreply.github.com>
2023-11-30 17:53:58 -0500

    Documentación 1

--- finalCode.v
+++ finalCode.v
@@ -1,36 +1,46 @@
+// Módulo finalCode con entradas y salidas
 module finalCode(coin,display,an,CLK_50,LED,rst);
-	input [4:0]coin;
-	input CLK_50;
-	input rst;
-	output [7:0]an;
-	output [3:0]LED;
-	output [6:0]display;
-	
-	reg [7:0]acarreo = 0;
-	integer i;
-	reg done;
+	// Entradas
+	input [4:0] coin; // Entrada de 5 bits para la moneda
+	input CLK_50; // Reloj de entrada de 50 MHz
+	input rst; // Señal de reset
+
+	// Salidas
+	output [7:0] an; // Salida para anodos, 8 bits
+	output [3:0] LED; // Salida para LEDs, 4 bits
+	output [6:0] display; // Salida para el display de 7 segmentos
+
+	// Declaración de variables internas
+	reg [7:0] acarreo = 0; // Acarreo, inicializado a 0
+	integer i; // Variable entera para iteraciones
+	reg done; // Registro para indicar finalización
 		
-	reg [27:0]contador;
-	reg [6:0]display;
-	reg [31:0]currency;
-	reg [4:0]comb;
-	reg [7:0]an;
+	reg [27:0] contador; // Contador de 28 bits
+	reg [6:0] display; // Registro para el display de 7 segmentos
+	reg [31:0] currency; // Registro para manejar la moneda, 32 bits
+	reg [4:0] comb; // Registro de combinación, 5 bits
+	reg [7:0] an; // Registro para anodos, 8 bits
 	
-	reg [24:0]clk;
-	
+	reg [24:0] clk; // Registro para manejar el reloj, 25 bits
+
+	// Asignación de LED a los 4 bits menos significativos de coin
 	assign LED = coin[3:0];
-	
+
+	// Bloque inicial para configurar valores iniciales
 	initial begin
-		an = 8'b11111110;
-		currency = 0;
-		clk = 0;
+		an = 8'b11111110; // Inicializar anodos
+		currency = 0; // Inicializar el valor de las monedas a 0
+		clk = 0; // Inicializar el reloj a 0
 	end
-	
+
+	// Este bloque siempre se ejecuta en el flanco positivo del reloj CLK_50.
 	always @(posedge CLK_50) begin
-		clk = clk + 1'b1;
+		clk = clk + 1'b1; // Incrementa el contador de reloj clk
 	end
-	
+
+	// Este bloque se ejecuta en el flanco positivo del bit 15 del contador clk.
 	always @(posedge clk[15]) begin
+		// Este conjunto de if-else rota los bits de 'an' para activar diferentes anodos en secuencia.
 		if (an==8'b11111110) an = 8'b11111101;
 		else if (an==8'b11111101) an = 8'b11111011;
 		else if (an==8'b11111011) an = 8'b11110111;
@@ -40,13 +50,15 @@ module finalCode(coin,display,an,CLK_50,LED,rst);
 		else if (an==8'b10111111) an = 8'b01111111;
 		else an = 8'b11111110;
 		
-		
+		// Bucle for para determinar qué anodo está activo actualmente.
 		for(i=7,done=0; i>=0 & done==0;i=i-1) begin
 			if (an[i]==0) begin
 				done = 1;
 			end
 		end
-		
+
+		// Esta sección actualiza el display en función del valor de la 'currency'.
+		// Cada if-else if comprueba el valor de 'i' para saber qué parte de 'currency' mostrar.
 		if (i==0) begin
 			case(currency[3:0])
 				4'b0000: display = 7'b1000000;
@@ -169,28 +181,34 @@ module finalCode(coin,display,an,CLK_50,LED,rst);
 		end
 	end
 		
+	// Bloque always que se ejecuta en cada flanco positivo del reloj CLK_50.
 	always @(posedge CLK_50) begin
-	
+		// Si se activa la señal de reset, reinicia el valor de las monedas a 0.
 			if (rst==0) currency = 0;
-	
+		
+		// Este bloque maneja la detección de monedas y el control de tiempo.
 		if (coin[0]==1'b1 | comb[0]==1'b1) begin
 			contador = contador + 1;
-			
+
+			// Reinicia el contador y comb después de un tiempo específico.
 			if (contador > 25_000_000) begin
 				contador = 0;
 				comb = 5'b00000;
 			end
-			
+
+			// Actualiza comb basado en la entrada de coin.
 			if (coin[0] == 1'b1) comb[0]=1'b1;
 			if (coin[1] == 1'b1) comb[1]=1'b1;
 			if (coin[2] == 1'b1) comb[2]=1'b1;
 			if (coin[3] == 1'b1) comb[3]=1'b1;
 			if (coin[4] == 1'b1) comb[4]=1'b1;
 			
-			//if (contador == 25_000_000) LED = ~comb;
-			
+			//if (contador == 25_000_000) LED = ~comb; // Prueba
+
+			// Lógica para actualizar currency y manejar el acarreo.
 			if (contador == 25_000_000) begin
-			
+
+				// Lógica para actualizar currency y acarreos
 				case(comb)
 					5'b00001 : begin
 						if (currency[3:0] == 0) currency = currency + 5;
@@ -245,6 +263,7 @@ module finalCode(coin,display,an,CLK_50,LED,rst);
 				
 				// ACARREOS _________________________________________________________________________________
 				
+				// Lógica para manejar acarreos en los diferentes segmentos de currency.
 				if (acarreo[0]==1) begin
 					if (~(currency[7:4]=='d9)) begin
 						currency[7:4] = currency[7:4] + 1;
@@ -314,6 +333,8 @@ module finalCode(coin,display,an,CLK_50,LED,rst);
 						acarreo[6] = 0;
 					end
 				end
+
+				// Reinicia el registro de acarreo una vez procesado.
 				acarreo = 0;
 			end	
 		end
